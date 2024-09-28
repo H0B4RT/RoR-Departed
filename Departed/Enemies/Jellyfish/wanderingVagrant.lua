f6c063e84b6a7cbb85186a2e32c78a01e89bfeec
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-09-28 00:22:35 -0500

    Added jellyfish model and wandering vagrant file

---
+++ Departed/Enemies/Jellyfish/wanderingVagrant.lua
@@ -0,0 +1,28 @@
+--[[
+Wandering Vagrant
+Appears throughout the game, from Classic to Survival to Providence Trials.
+Is One of the Tier 1 Bosses
+Acts kind of like a hybrid between RORR and ROR2
+Has Two Boss Items
+Primarily ranged attacks BUT it now shocks on contact.
+Is a Hybrid* Boss (Ranged, Melee)
+
+Health: 1400
+Damage: 12
+Speed: 22 KM/H
+Armor: 0
+Title: Gentle Protector
+Type: Boss
+Tier: 1
+Skills:
+Passive: Electrostatic
+On contact, deal 50% damage.
+Primary: Orb Volley
+Fire six orbs at the nearest enemy over 2 seconds, dealing 6x200% damage. Highlights where orbs will land.
+CD: 10s
+Special: Genesis Supernova
+Glow brightly and charge for 5 seconds, then release a incredible explosion that deals 1000% damage. CAN ONLY USE WHEN BELOW 20% HEALTH.
+CD: 15s
+Boss Items:
+Nematocyst Nozzle: Shoot out 6 homing nematocysts that deal 400% damage.
+--]]
