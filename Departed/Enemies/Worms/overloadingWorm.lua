f174635f094f279f991256b5d9f6d327eb805a56
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-09-28 00:42:20 -0500

    Added worm boss files

---
+++ Departed/Enemies/Worms/overloadingWorm.lua
@@ -0,0 +1,29 @@
+--[[
+Overloading Worm
+Appears late into runs in Classic and past wave 50 in Survival.
+Key Notes
+Is One of the Tier 3 Bosses
+Acts similarly to the ROR1/RORR version, jumping very high into the air and speeding up the lesser health it has
+Has 1* Boss Item
+CANNOT BE ELITE
+Is a Hybrid Boss (Melee, Ranged)
+
+Health: 3920
+Damage: 105
+Speed: 268 KM/H
+Armor: 0
+Title: The Reminder
+Type: Boss
+Tier: 3
+Skills:
+Passive: Overloading
+On contact, deal 50% damage. Inherit Adrenaline and the abilities of an Overloading Elite.
+Primary: Jump and Dive
+Erupt from the ground dealing 50% damage to what you hit. Then, dive back down and deal 100% damage to what you hit. Leave an AOE that inflicts burn in an area. 
+CD: 5s
+Misc: Thunderstorm
+Whilst above ground, cause a Thunderstorm that strikes the area around the Overloading Worm that deals 500% damage per lightning bolt.
+CD: NONE, HAS TO BE ABOVE GROUND TO BE ACTIVE
+Boss Item:
+Charged Perforator: 10% chance to call down a lightning strike, dealing 1000% (+1000% per stack) damage.
+--]]
---
+++ Departed/Enemies/Worms/volatileWorm.lua
@@ -0,0 +1,29 @@
+--[[
+Volatile Worm
+Appears late into runs in Classic and past wave 50 in Survival.
+Key Notes
+Is One of the Tier 3 Bosses
+Acts similarly to the ROR1/RORR version, jumping very high into the air and speeding up the lesser health it has
+Has 1* Boss Item
+CANNOT BE ELITE
+Is a Hybrid Boss (Melee/Ranged)
+
+Health: 3920
+Damage: 105
+Speed: 268 KM/H
+Armor: 0
+Title: The Tempermental
+Type: Boss
+Tier: 3
+Skills:
+Passive: Volatile
+Upon death, release spite bombs. Inherit Adrenaline and the abilities of a Volatile Elite.
+Primary: Jump and Dive
+Erupt from the ground dealing 50% damage to what you hit. Then, dive back down and deal 100% damage to what you hit. Leave an AOE that inflicts burn in an area. 
+CD: 5s
+Misc: Bombogenesis
+Whilst above ground, cause a Explosive Cyclone around the Worm that causes exploding fireballs to fall around the Volatile Worm.
+CD: NONE, HAS TO BE ABOVE GROUND TO BE ACTIVE
+Boss Item:
+Eruptive Perforator: 10% chance to call forth 5 magma balls, dealing 200% (+200% per stack) damage.
+--]]
