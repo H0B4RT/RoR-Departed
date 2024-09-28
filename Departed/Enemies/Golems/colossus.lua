9c325170ead00cbc8f8d573499cd7cebef1104d2
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-09-28 00:17:50 -0500

    Added colossus file and golem model

---
+++ Departed/Enemies/Golems/colossus.lua
@@ -0,0 +1,36 @@
+--[[
+Colossus
+Appears throughout the game, from Classic to Survival to Providence Trials.
+Key Notes
+Is One of the Tier 1 Bosses
+Acts almost identically to the RORR Colossus, with one additional move/ability
+Has Two Boss Items
+Can aim high with its attacks, so being above it doesn’t guarantee you will be safe
+Is a Melee Boss
+
+Health: 1400
+Damage: 40
+Speed: 14 KM/H
+Armor: 0
+Title: Giant Stone Guardian
+Type: Boss
+Tier: 1
+Skills:
+Passive: Mountain King
+Upon spawning, summon two Stone Golems. 
+If Blazing, summon two Blazing Wisps instead.
+If Overloading, summon a row of Overloading Jellyfish instead.
+Primary: Stone Clap
+Clap towards the nearest enemy for 100% damage. Summon a Stone Golem. Can aim high or low.
+If Blazing, summon a Blazing Wisp instead.
+If Overloading, summon an Overloading Jellyfish instead.
+Secondary: Stone Overcut
+Throw an overhand punch downwards for 100% damage. Only use when an enemy is above center point. If enemy is above center point, spike downwards. If enemy is below center point, deal 25% damage instead.
+Utility: Earthquake
+Stomp the ground, causing a shockwave that deals 200% damage. 
+If Volatile, shockwave increases in size and explodes.
+Boss Items:
+Colossal Knurl: Increase maximum health by 8%, health regeneration by 1.2 hp/s, and armor by 6.
+Guardian’s Fist: Upon use, two Colossus hands clap for 100% damage of a COLOSSUS’ BASE DAMAGE.
+CD: 8s
+--]]
