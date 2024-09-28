84aa7ad30695428fad3bf1a5e39b68a478274cec
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-09-28 00:09:30 -0500

    added construct models and cryptid lua

---
+++ Departed/Enemies/Constructs/cryptid.lua
@@ -0,0 +1,33 @@
+--[[
+Major Construct
+Appears in the 50th Wave of Survival and appears regularly from that point forward
+Key Notes
+DEPARTED EXCLUSIVE CONTENT
+Is a Boss that only appears in survival (unless changed at a later point)
+Acts similarly to the unused Major Construct from Risk of Rain 2
+Has its own Boss Item
+Summons Beta Constructs (ask Retro)
+Is a Hybrid Boss (Support, Ranged)
+
+Health: 1800
+Damage: 30
+Speed: 0
+Armor: 30
+Title: Crisis Vanguard
+Type: Boss
+Tier: 4 (Special)
+Skills:
+Primary: Major Cannon 
+Charge for 1 second and then fire off 3 fast projectiles in a triangle formation that deal 3x150% damage. Has end lag of 0.5 seconds.
+Secondary: Major Barrier
+Create a large, impenetrable shield (about 24 meters). Does not stop allies or enemies from walking through the walls of the shield. Lasts for 15s. Only one can be active at a time.
+CD: 30s
+Utility: Relocate
+CAN ONLY BE ACTIVATED WHEN AT LEAST 100M AWAY FROM PLAYER. Becomes intangible and buries itself underground. Reappears within 20M of the nearest player and chocks up little rocks that stun enemies. Has 2 seconds of end lag.
+CD: 15s
+Special: Tetra Beta
+Spawn 4 Beta Construct allies. Can have up to 8 Beta Construct allies at a time.
+CD: 30s
+Boss Item:
+Hostile Nucleus: Upon being hit by an enemy, reduce damage by 50% (+50%). Deal the same percentage of damage back to attacker. After reflecting 100% percent damage, spawns an ally Beta Construct.
+]]--
