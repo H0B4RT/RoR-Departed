ba5463657d886f1ce456dcc71220a217e0c64e5d
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-09-29 01:12:27 -0500

    Add files via upload

---
+++ Departed/Enemies/Golems/stoneTitan.lua
@@ -0,0 +1,23 @@
+--[[
+Stone Titan
+Appears late into Classic and Survival
+Key Notes
+	- DEPARTED CONTENT
+	- Appears on any stage that Stone Golem or Colossus spawn post loop, Detritus, Black Beach
+	- Acts similarly to how it is in RoR2, but has a primary melee attack instead of a utlity.
+	- Primary can aim high or low, secondary can come out of the ceiling*, eye laser is eye laser
+	- Is a Hybrid (Melee/Ranged) Enemy
+	
+Health: 900
+Damage: 37
+Speed: 18 KM/H
+Armor: 0
+Type: Grand Champion Enemy
+Skills:
+	- Primary: Titanous Trounce (lol) *
+		- Stone Titan haymakers dealing 150% damage.
+	- Secondary: Stone Fist
+		- Stone Titan plunges its fist into the ground, highlighting an area near the player, and then deals 120% damage.
+	- Special: Eye Laser
+		- Stone Titan charges up a laser and then fires, dealing 24 x 12.5% damage over 3 seconds.
+--]]
