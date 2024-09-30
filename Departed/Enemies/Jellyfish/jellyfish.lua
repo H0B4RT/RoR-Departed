e46866561db4c5a6f5087b916adea40f302af229
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-09-29 21:22:23 -0500

    Added jellyfish family files

---
+++ Departed/Enemies/Jellyfish/jellyfish.lua
@@ -0,0 +1,25 @@
+--[[
+Jellyfish
+Appears throughout the game, from Classic to Survival to Providence Trials.
+Key Notes
+	- BASE CONTENT
+	- Appears on Stage 1 besides Slate Mines, and Sky Meadow
+	- Acts like a combination of all three games
+	- Charges in and explodes at lower health.
+	- Is a Melee Enemy
+	
+Health: 85
+Damage: 7
+Speed: 24 KM/H
+Armor: 0
+Type: Regular Enemy
+Skills:
+	- Passive: Electrostatic
+		- On contact, deal 50% damage.
+	- Utility: Rush
+		- Charge towards the nearest enemy, increasing speed by 16 KM/H for 2 seconds.
+			- CD: 7s
+	- Special: Unstable Nova
+		- Charge up for 1 second and release an explosion dealing 400% damage. Deals a quarter of the damage to you. ONLY USABLE AT UNDER 50% HEALTH.
+			- CD: 5s
+--]]
--- Departed/Enemies/Jellyfish/wanderingVagrant.lua
+++ Departed/Enemies/Jellyfish/wanderingVagrant.lua
@@ -1,28 +1,35 @@
 --[[
 Wandering Vagrant
 Appears throughout the game, from Classic to Survival to Providence Trials.
-Is One of the Tier 1 Bosses
-Acts kind of like a hybrid between RORR and ROR2
-Has Two Boss Items
-Primarily ranged attacks BUT it now shocks on contact.
-Is a Hybrid* Boss (Ranged, Melee)
+Key Notes
+	- BASE CONTENT
+	- Is One of the Tier 1 Bosses
+	- Appears on Stage 1 besides Slate Mines, Sunken Tomb, Hidden Sanctum, Verdant Woodland, Uncharted Mountain, and Black Beach
+	- Acts kind of like a hybrid between RORR and ROR2
+	- Has Two Boss Items
+	- Primarily ranged attacks BUT it now shocks on contact.
+	- Is a Hybrid* Boss (Ranged, Melee)
 
 Health: 1400
 Damage: 12
-Speed: 22 KM/H
+Speed: 20 KM/H
 Armor: 0
 Title: Gentle Protector
 Type: Boss
 Tier: 1
 Skills:
-Passive: Electrostatic
-On contact, deal 50% damage.
-Primary: Orb Volley
-Fire six orbs at the nearest enemy over 2 seconds, dealing 6x200% damage. Highlights where orbs will land.
-CD: 10s
-Special: Genesis Supernova
-Glow brightly and charge for 5 seconds, then release a incredible explosion that deals 1000% damage. CAN ONLY USE WHEN BELOW 20% HEALTH.
-CD: 15s
+	- Passive: Electrostatic
+		- On contact, deal 50% damage.
+	- Primary: Orb Volley
+		- Fire six orbs at the nearest enemy over 2 seconds, dealing 6x200% damage. Highlights where orbs will land.
+			- CD: 6s
+	- Utility: Lesser Burst
+		- Charge towards the nearest enemy, increasing speed by 4 KM/H for 2 seconds.
+			- CD: 7s
+	- Special: Genesis Supernova
+		- Glow brightly and charge for 5 seconds, then release a incredible explosion that deals 1000% damage. CAN ONLY USE WHEN BELOW 20% HEALTH.
+			- CD: 15s
 Boss Items:
 Nematocyst Nozzle: Shoot out 6 homing nematocysts that deal 400% damage.
+Brain Coral: Gain a 15% (+15% per stack) increase to a random stat.
 --]]
