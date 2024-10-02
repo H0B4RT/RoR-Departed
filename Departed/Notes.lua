24f50740d2c084844af1426e979212dcf7dd3d43
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-10-02 16:48:32 -0500

    added some notes i made about ratios and what not

---
+++ Departed/Notes.lua
@@ -0,0 +1,27 @@
+--[[
+Some general notes about equality and what not.
+RoRR has 
+16 Survivors (1 Secret)
+FOR
+142 items (61 unlockable) - 8.875 items per survivor (3.8125 unlockables)
+10 Stages - 0.625 stages per survivor [1.6 survivors per stage]
+34 Enemies (5 Special) - 2.125 enemies per survivor (0.3125 special) [3.2 survivors per special enemy]
+16 Bosses (6 Special) - 1 boss per survivor (0.375 special) [2.6^ survivors per special boss]
+
+3.4 enemies per stage
+1.6 bosses per stage
+RoR2 has
+17 survivors (1 secret)
+FOR
+208 items (46 unlockable) 12.235 per survivor
+28 stages (11 hidden) 1.647 stages per survivor (0.647 hidden) [1.54^ survivors per hidden stage]
+38 Enemies (4 Special) 2.2
+23 Bosses (10 Special)
+
+RoRD (First 2 Updates) has
+13 survivors (1 secret)
+40 + 37 + 29 + 15 + 33
+1 Stage (SHOCK AND HORROR)
+17 Enemies (3 Special)
+7 Bosses (1 Special)
+--]]
