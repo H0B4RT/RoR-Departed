bca744a68230022b76c119d5f68c1971640cf970
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-10-02 10:11:42 -0500

    Updated sandCrabKing.lua

--- Departed/Enemies/Crabs/sandCrabKing.lua
+++ Departed/Enemies/Crabs/sandCrabKing.lua
@@ -1,14 +1,13 @@
 --[[
 Sand Crab King
-
 Appears throughout the game, from Classic to Survival to Providence Trials.
 Key Notes
-STARSTORM DLC EXCLUSIVE BOSS
-Is One of the Tier 1 Bosses
-Acts almost identically to the SS1 Sand Crab King
-Has Two Boss Items
-Has piercing attacks that have fairly large ranges
-Is a Hybrid Boss (Melee, Ranged)
+    - STARSTORM DLC EXCLUSIVE BOSS
+    - Is One of the Tier 1 Bosses
+    - Acts almost identically to the SS1 Sand Crab King
+    - Has Two Boss Items
+    - Has piercing attacks that have fairly large ranges
+    - Is a Hybrid Boss (Melee, Ranged)
 
 Health: 1200
 Damage: 30
@@ -18,15 +17,15 @@ Title: Hardened Monstrosity
 Type: Boss
 Tier: 1
 Skills:
-Primary: Crush
-Reach out and crush the nearest enemy in your claws, dealing 150% damage.
-Secondary: Puncture
-Quickly stab your claw into the ground, dealing 100% damage in an area.
-Special: Entrapping Bubbles
-Release bubbles. If an enemy makes contact with a bubble, trap them in the bubble and lock their skills.
-If Volatile, release twice as many bubbles.
+    - Primary: Crush
+        - Reach out and crush the nearest enemy in your claws, dealing 150% damage.
+    - Secondary: Puncture
+        - Quickly stab your claw into the ground, dealing 100% damage in an area.
+    - Special: Entrapping Bubbles
+        - Release bubbles. If an enemy makes contact with a bubble, trap them in the bubble and lock their skills.
+            - If Volatile, release twice as many bubbles.
 Boss Items:
 Shell Piece: Gain immunity for 3 seconds after being lethally hit. Only once per stage (+1 per stack). Afterwards, gain 11 armor for the rest of the stage.
 Bubble Blower: Release bubbles that trap enemies and prevent them from using their skills.
-CD: 15s
+    CD: 15s
 ]]--
