4de7b0cab29b866be2c64b163991075e75f4c2ad
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-10-02 14:46:28 -0500

    WOW ! ! ! ENEMY TEMPLATE NEO!

---
+++ Departed/Enemies/Enemy Template.lua
@@ -0,0 +1,27 @@
+--[[
+Enemy Name
+Where It Appears
+Key Notes
+	- What content pack its apart of
+	- What tier it is (Boss)
+	- What stages it appears on
+	- How it acts, was it reworked, etc
+	- How many boss items (Boss)
+	- Quick recap of skills and capabilities
+	- What kind of enemy is it? (Melee, Ranged, Support, Hybrid, Boss, Enemy)
+	
+Health: 0
+Damage: 0
+Speed: 0 KM/H
+Armor: 0
+Type: Special Enemy
+Skills:
+	- Primary: 
+	- Secondary: 
+	- Utility:
+	- Special:
+	- Misc:
+Boss Item:
+	- Boss Item 1:
+	- Boss Item 2:
+--]]
