2027b1ae6eabf3aceca3f0e580d73d7fc8c58af7
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-10-17 11:19:21 -0500

    Made Ancient Wisp file more in line with other bosses

--- Departed/Enemies/Wisps/ancientWisp.lua
+++ Departed/Enemies/Wisps/ancientWisp.lua
@@ -2,11 +2,11 @@
 Ancient Wisp
 Appears throughout the game, from Classic to Survival to Providence Trials.
 Key Notes
-Is one of the Tier 2 Boss
-Acts very similarly to the unused ROR2 version of the Ancient Wisp, with elements of the ROR1/R version
-Has Two Boss items
-Does not have a lot of skills, but can generally dish out a lot of damage anyway.
-Is a Ranged Boss
+    - Is one of the Tier 2 Boss
+    - Acts very similarly to the unused ROR2 version of the Ancient Wisp, with elements of the ROR1/R version
+    - Has Two Boss items
+    - Does not have a lot of skills, but can generally dish out a lot of damage anyway.
+    - Is a Ranged Boss
 
 Health: 1470
 Damage: 23
@@ -16,19 +16,19 @@ Title: Banished and Chained
 Type: Boss
 Tier: 2
 Skills:
-Passive: Revenge
-Upon death, summon 2 Greater Wisps and 2 Lesser Wisps.
-Post Loop, summon 2 Archaic Wisps and 2 Greater Wisps.
-Secondary: Lightning Storm
-Summon 8 telegraphed lightning bolts for 8x100% damage.
-If Volatile, summon twice as many lightning bolts.
-If Frenzied, half the cooldown of this skill.
-CD: 8s
-Special: Enrage
-Increase damage, attack speed, and cooldown reduction of Lightning Storm by 50%. Also summon twice as many lightning bolts. Effect lasts 12 seconds.
-CD: 60s
+    - Passive: Revenge
+        - Upon death, summon 2 Greater Wisps and 2 Lesser Wisps.
+            - Post Loop, summon 2 Archaic Wisps and 2 Greater Wisps.
+    - Secondary: Lightning Storm
+        - Summon 8 telegraphed lightning bolts for 8x100% damage.
+            - If Volatile, summon twice as many lightning bolts.
+            - If Frenzied, half the cooldown of this skill.
+            - CD: 8s
+    - Special: Enrage
+        - Increase damage, attack speed, and cooldown reduction of Lightning Storm by 50%. Also summon twice as many lightning bolts. Effect lasts 12 seconds.
+            - CD: 60s
 Boss Items:
 Legendary Spark: 8% chance on hit to create 2 (+1 per stack) sparks that smite enemies for 200% TOTAL damage. 
 Legendary Mask: On use, summon an Archaic Wisp with 200% Health and Damage. Can have up to 2 Archaic Wisps at a time.
-CD: 140s
+    - CD: 140s
 --]]
