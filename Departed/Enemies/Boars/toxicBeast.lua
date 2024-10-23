01376364b03f7645430bfc19dd5140545d7cd411
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-10-23 08:57:13 -0500

    Updated toxicBeast.lua

---
+++ Departed/Enemies/Boars/toxicBeast.lua
@@ -0,0 +1,28 @@
+--[[
+Toxic Beast
+Appears throughout the game from Classic to Survival to Providence Trials
+Key Notes
+	- BASE CONTENT
+    - Is a Tier 2 Boss
+    - Appears on Boar Beach, Magma Barracks, Hive Cluster, and Temple of the Elders
+    - Works almost identically to its RoRR counterpart, except it uas a small chance to summon a Boar instead of a Boarlit
+	- Has two Boss Items
+    - Tramples and Scream
+    - Can Walk, Can Jump, Can Fall
+    - Is a Hybrid Enemy (Melee/Support)
+	
+Health: 1460
+Damage: 17
+Speed: 33 KM/H
+Armor: 0
+Family: Boar
+Title: Mother of Many
+Type: Boss
+Tier: 2 (Mid-Game)
+Skills:
+	- Passive: Trample
+	- Special: Mother's Squeal
+Boss Item:
+    - Toxic Tail:
+    - Fetid Tongue:
+--]]
