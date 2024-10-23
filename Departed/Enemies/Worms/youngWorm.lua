410bff44e1468b82eae637436ba90f9ec4cd6c04
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-10-23 08:36:51 -0500

    Add youngWorm.lua

---
+++ Departed/Enemies/Worms/youngWorm.lua
@@ -0,0 +1,23 @@
+--[[
+Young Worm
+Appears late into Survival and Classic
+Key Notes
+	- Appears on Torrid Outlands, and any stage where Magma Worm, Overloading Worm, or Volatile Worm spawn post loop.
+	- Acts like a Mini Magma Worm.
+    - Can only be Blazing, Overloading, or Volatile
+	- Skill Based Movement
+	- Is a Melee Enemy
+
+Health: 235
+Damage: 20
+Speed: 33 KM/H
+Armor: 0
+Family: Worm
+Type: Grand Champion
+Skills:
+	- Passive: Adrenaline
+		- Speed and cooldown reduction are increased by 10% for every 20% Health missing.
+	- Primary: Leap and Dive
+		- Erupt from the ground dealing 50% damage to what you hit. Then, dive back down and deal 100% damage to what you hit. Leave an AOE that inflicts burn in an area.
+			- CD: 2s
+--]]
