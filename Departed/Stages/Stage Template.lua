cea269e160d21e7c5aa167507eeec5e5d789374f
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-10-25 22:25:58 -0500

    GIVE ME THAT EMERALD... OR ELSE!

---
+++ Departed/Stages/Stage Template.lua
@@ -0,0 +1,33 @@
+--[[
+Stage Name
+Mode it appears in
+Key Notes
+	- Stage Title
+	- Stage Number
+	- Stage Description
+	- Stage Ambient Song
+	- Stage Teleporter Song
+	- Additional Notes
+
+Variant Number:
+Artifact: 
+Endemic Life:
+
+Enemies:
+Bosses:
+Interactables:
+
+Post Loop
+
+Enemies:
+Bosses:
+Interactables:
+
+Stage Director Interactable Credits: 0
+Stage Director Enemy Credits: 0
+
+Possible Family Events:
+	- A Family of Five
+	
+Log:
+--]]
---
+++ Departed/Stages/Survival/Black Beach/blackBeach.lua
@@ -0,0 +1,31 @@
+--[[
+Black Beach
+Survival
+Key Notes
+	- Forsaken Isle
+	- Survival Stage
+	- A very large island in the middle of an alien ocean, perfectly isolated from the rest of the planet. If one crash lands here, there is no way to go to the rest of the planet besides boat.
+	- Stage Ambient Song
+	- Stage Teleporter Song
+	- Will be constantly updated, EVERY SURVIVOR SHOULD EXCEL SOMEHERE ON BLACK BEACH.
+	- Gives access to the Exile ending before the Contact Light gets added.
+
+Variant Number: 1
+Artifact: Evolution
+Endemic Life: ALL
+
+Enemies: ALL
+Bosses: ALL
+Interactables:
+
+Stage Director Interactable Credits: 550
+Stage Director Enemy Credits: 100, actively increases by 50 every 20 waves.
+
+Possible Family Events:
+	- Any
+
+Log:
+Strange how still it is here. The waves barely make a sound, and these broken ruins scattered along the shore hint that someone else tried to make this place home once, too. It’s almost peaceful, in a way. Maybe I can last here a while, learn the tides, maybe even find something to eat inland. It’s quiet… isolated, sure, but if I’m here for the long haul, I might as well make it livable. 
+
+Not the worst place to land, all things considered.
+--]]
