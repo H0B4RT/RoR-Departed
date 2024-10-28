a0b06e1237b90362fd4a53873e97767b9fe950b9
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-10-28 10:19:29 -0500

    Added specialist

---
+++ Departed/Survivors/Commando/Variants/specialist.lua
@@ -0,0 +1,33 @@
+--[[
+Specialist
+Key Notes
+    - The Specialist is equipped with a Heavy Pistol for delivering piercing damage on his enemies at the cost of a slower firing rate.
+	- Variant Of: Commando
+	- Variant Type: Regular
+	- Umbra Title: TBD
+	- Exile Quote: TBD
+
+Unlock: As Commando, reach the 4th stage in under 15 minutes.
+
+Health: 112 (+34 per level)
+Regen: 1.2 (+0.7 per level) HP/S
+Damage: 12 (+3 per level)
+Speed: 28 KM/H
+Armor: 0 (+1 per level)
+Skills:
+    - Passive: Cardio
+    - Primary: Deadeye
+        - Fire a heavy pistol bullet which pierces for 165% damage. 
+            - CD: 0.5s
+	- Secondary: Dirt Bomb
+        - Launch a dirt bomb which stuns and deals 310% damage on exploding.
+            - CD: 4.5s
+	- Utility: Tactical Dive
+	- Special: Frag Grenade
+        - Throw a grenade that explodes for 700% damage. Can hold up to 2.
+            - CD: 5s
+            - Charges: 2
+		- Ancient Scepter: Carpet Bomb
+            - Throw a grenade that splits apart and explodes for 4x300% damage. Can hold up to 2.
+
+--]]
