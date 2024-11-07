e02e76bc848aae16ff4c7e56d6956d9bebc86b51
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-11-06 21:20:35 -0600

    Added wayfarer.lua, LoM might be renamed…

---
+++ Departed/Enemies/Post/wayfarer/wayfarer.lua
@@ -0,0 +1,54 @@
+--[[
+Wayfarer
+Appears throughout the game, from Classic to Survival to Providence Trials
+Key Notes
+    - STARSTORM CONTENT
+    - Is a Tier 1 (Early-Game) Boss
+    - Appears on Slate Mines, Sky Meadow, Whistling Basin, Uncharted Mountain, and Black Beach
+    - Blend of SS1 and SS2, slightly reworked.
+    - Has two Boss Items
+    - Lamp Smash, Haunted Projectile, Invisible Thingy, Buff
+	- Can Walk, Jump, Fall
+	- Standard: Has a 45% chance to drop a Common Item, a 30% chance to drop an Uncommon Item, a 10% chance to drop a Legendary, and 15% chance to drop one of the Boss' respective Boss Item(s).
+    - Is a Hybrid Enemy (Melee/Ranged/Support)
+	
+Health: 1400
+Damage: 26
+Speed: 17 KM/H
+Armor: 0
+Family: Post
+Title: Usher of Light
+Type: Boss
+Tier: 1 (Early-Game)
+Skills:
+	- Primary: Lamp Smash
+        - Smash your two lamps together, dealing 100% damage in a small area.
+            - CD: 4s
+	- Secondary: Haunted Barrage
+        - Fire off a barrage of slightly homing projectiles, dealing 6x50% damage.
+            - CD: 6s
+	- Utility: Haunt
+        - Turn invisible and teleport within 30m of the nearest enemy. CANNOT TELEPORT ON TOP OF ENEMY.
+            - CD: 10s
+	- Special: Guiding Light
+        - Raise a lamp and rally allies, increasing movement speed and attack speed by 25% and allowing all allies that can walk around you to jump and fall.
+            - CD: 20s
+Boss Item:
+	- Unearthly Lamp: Every second use of a primary attack fires 1 (+1 per stack) haunted projectile dealing 100% (+100% per stack) damage.
+	- Lantern of Minerva: Unleash a beam of light in front of you, dealing 50% damage and inflicting burn every half second. Lasts up to 5s. Additionally, reveals enemies and interactables not in direct line of sight.
+        - CD: 60s
+	
+Director Cost: 325
+Spawn Group: 1
+Weight: 1
+Stage Condition: 1
+Wave Condition: 1
+
+Log:
+Field Notes:  
+What I saw, was familiar, it had close resemblance to an old light post, however, it casted a cold, eerie light that felt like the presence of more than what my eyes could perceive.
+
+While possessing what seem to be legs, it doesn't walk. Instead, it hovers over the ground in a very mystical manner.
+
+Other creatures from the planet seem to be comforted around this entity, a kind of comfort that feels as if it had been stripped away from me the very moment I faced this being.
+--]]
