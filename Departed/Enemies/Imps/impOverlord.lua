1eac440bb0f0b6d27f0b07ba32cea142e9fe7a71
Hobarticus "Whimsical Silly Road" Goobius <thewindowsxpleviathan@gmail.com>
2024-12-05 12:23:32 -0600

    Departed is back on the menu.

---
+++ Departed/Enemies/Imps/impOverlord.lua
@@ -0,0 +1,50 @@
+--[[
+Enemy Name
+Where It Appears
+Key Notes
+	- BASE CONTENT
+    - Is a Tier 1 (Early-Game) Boss
+    - Appears on Slate Mines, Whistling Basin, Ancient Valley, Sunken Tomb, Scorched Acres, Hive Cluster, Magma Barracks, and Temple of the Elders
+    - Acts very similarly to the RoRR counterpart with the RoR2 teleport
+	- Has two boss items
+	- Swipe, charge, teleport
+	- Can Walk, Jump, Fall, and Teleport
+	- Standard: Has a 45% chance to drop a Common Item, a 30% chance to drop an Uncommon Item, a 10% chance to drop a Legendary, and 15% chance to drop one of the Boss' respective Boss Item(s).
+    - Is a Melee Boss
+	
+Health: 1210
+Damage: 27
+Speed: 27 KM/H
+Armor: 0
+Family: Imp
+Title: Lord of the Red Plane
+Type: Boss
+Tier: 1 (Early-Game)
+Skills:
+	- Primary: Swipe
+        - Attacks 3 times directly in front of it for 3x100% damage.
+            - CD: 2s
+    - Secondary: Lunge
+        - Dashes forwards and slashes 3 times, dealing 3x100% damage.
+            - CD: 6s
+	- Utility: Teleport
+        - Disappears and reappears near the nearest player.
+            - CD: 10s
+	- Special: Laser Vapor
+        - Screams and then traces a red line in front of this and then unleashes red vapor, dealing 10x50% damage.
+Boss Item:
+    - Imp Overlord's Tentacle: Summon an imp bodyguard. Revives after 60 (-10 per stack) seconds. Increase imp health and damage by 15% per stack.
+    - Imp Vanguard's Spleen: Gain 5% critical chance. Critical Strikes bleed enemies for 240% base damage. Bleeding enemies explode on death for 400% (+400% per stack) damage, plus an additional 15% (+15% per stack) of their maximum health.
+	
+Director Cost: 350
+Spawn Group: 1
+Weight: 1
+Stage Condition: 1
+Wave Condition: 1
+
+Log:
+Field Notes:
+Oh, why me? My heart stopped when this great Imp manifested in previously empty space. So many horrendous eyes. With a gaping scream the Imp lets out a deadly red vapor. Some type of laser also trails me.
+
+This demon is just as baffling in its movement as the smaller one. Barely escaping the Imp's whipping grasp I landed a final blow. Its dying body split open at its meridian, displaying its innards like some ghastly flower. This monster is just as gruesome in death as it was in life.
+--]]
