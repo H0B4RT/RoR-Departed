954ff15a415b71d821b82df1aab4d5cb3421b016
H0B4RT <thewindowsxpleviathan@gmail.com>
2025-01-24 10:42:29 -0600

    added some models hopefully, been a while since i used git

---
+++ Departed/Enemies/Mutator.lua
@@ -0,0 +1,35 @@
+--[[
+Mutator
+Where It Appears
+Key Notes
+	- What content pack its apart of (BASE CONTENT, DEPARTED CONTENT, STARSTORM CONTENT, FUTURE CONTENT)
+	- What stages it appears on
+	- How it acts, was it reworked, etc
+	- Quick recap of skills and capabilities
+	- Movement Options (Walk, Jump, Fall, Climb, Fly, Teleport, Skill Based, None)
+	- What kind of enemy is it? (Melee, Ranged, Support, Hybrid)
+	- Additional Notes
+	
+Health: 0
+Damage: 0
+Speed: 0 KM/H
+Armor: 0
+Family: Family Name Here
+Type: Regular Enemy, Champion Enemy, Grand Champion Enemy, Misc Enemy, Special Enemy
+Skills:
+	- Passive:
+	- Primary: 
+	- Secondary: 
+	- Utility:
+	- Special:
+	- Misc:
+	
+Director Cost:
+Spawn Group: 
+Weight:
+Stage Condition: 
+Wave Condition:
+
+Log:
+TBD
+--]]
