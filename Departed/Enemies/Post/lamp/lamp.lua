dc72fc2f2aed02496438d37c411da8f50be9de70
H0B4RT <h0b4rt@gmail.com>
2025-01-29 23:16:11 -0600

    THE LAMPS OF THE WATCHER
    added the models and lua files for the follower and watcher

---
+++ Departed/Enemies/Post/lamp/lamp.lua
@@ -0,0 +1,37 @@
+--[[
+Follower
+Appears throughout the game, from Classic to Survival to Providence Trials
+Key Notes
+	- Appears throughout the game, from Classic to Survival to Providence Trials
+	- Appears on Slate Mines, Sky Meadow, Whistling Basin, Uncharted Mountain, and Black Beach
+	- Reworked to be a slow moving, bulky buffing flier, but with the attacks from SS1.
+	- Homing Projectile, Buff
+	- Can Fly
+	- Is a Hybrid Enemy (Ranged/Support)
+	
+Health: 450
+Damage: 20
+Speed: 17 KM/H
+Armor: 0
+Family: Post
+Type: Champion Enemy
+Skills:
+	- Primary: Haunted Projectile
+		- If no ally is around, fire a homing projectile for 100% damage.
+			- CD: 4s
+	- Special: Guide
+		- Rally an ally, increasing movement speed and attack speed by 25% and allowing them to jump and fall. Lasts 5s.
+			- CD: 10s
+	
+Director Cost: 75
+Spawn Group: 1
+Weight: 1
+Stage Condition: 1 
+Wave Condition: 5
+
+Log:
+Field Notes:
+With these around, darkness isn't my main concern. Not quite because of their vigorous light, but rather their obsessive, incessant interest in living beings. I'm still not entirely sure how could a species evolve to resemble... a lamp? Is it even organic?
+
+Of all its quirks, it caught my attention that, the Follower, as I have come to call it, isn't aggressive towards me. That is, unless there is no other kind of creature around it can follow. Despite being a light source, it is no guide. Just a follower.
+--]]
---
+++ Departed/Enemies/Post/menorah/menorah.lua
@@ -0,0 +1,44 @@
+--[[
+Watcher
+Appears late into Classic and Survival
+Key Notes
+	- STARSTORM CONTENT
+	- Appears on Uncharted Mountain, Black Beach, and any stage that Followers or Wayfarer spawn on post loop.
+	- Living Warbanner, buffs armor as well.
+	- Passive buff to allies near it, Projectile, Healing Nova
+	- Immobile
+	- Is a Hybrid Enemy (Ranged/Support)
+	- Idea from Glasus (SOMEBODY GOT A MENORAH FOR ME?)
+	
+Health: 900
+Damage: 25
+Speed: 0 KM/H
+Armor: 0
+Family: Post
+Type: Grand Champion Enemy
+Skills:
+	- Passive: Phare
+		- Whilst the Watcher is alive, it rallies allies in a 24 meter radius. This increases their attack speed and movement speed by 25% and allows them to jump and fall. When allies exit the radius, the buff lasts for 5 seconds. DOES NOT APPLY TO OTHER WATCHERS.
+	- Primary: Haunted Volley (?)
+		- Fire off two homing projectiles, dealing 2x100% damage.
+			- CD: 2s
+	- Special: Mediarama
+		- Heal allies within a 16 meter radius.
+			- CD: 15s
+	
+	
+Director Cost: 175
+Spawn Group: 1
+Weight: 1
+Stage Condition: 5
+Wave Condition: 50
+
+Log:
+Awwwwwww shit, here we go! Somebody got a yellow item for me!?
+
+Heeeyyyy, there's the bastard right now!
+
+Fork over the goddamn lantern or I'll smear your GUTS-E across the road!
+
+This is not the final log, I just am not in the writing mood right now.
+--]]
