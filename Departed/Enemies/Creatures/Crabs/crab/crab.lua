c86d4b48cc7b01141b3f28c8c48108a534ae9ce6
Hobarticus "Whimsical Silly Road" Goobius <h0b4rt@gmail.com>
2025-07-28 07:41:35 -0500

    Added endemic life stuff. Probs need to find a new name for this kind of creature.

---
+++ Departed/Enemies/Creatures/Crabs/crab/crab.lua
@@ -0,0 +1,25 @@
+--[[
+Crab
+ENDEMIC LIFE
+Key Notes
+	- Endemic life are creatures that aren't typical enemies. They won't go out of their way to attack the player, and rather just exist in an area around the map. They won't respawn when killed, think the Birdsharks on Distant Roost. They are there to provide lore and information about an area. 
+	- Appears on Dried Lake near the edges of the map, appears on Black Beach near the water.
+	- A skittish lil crab, implied to be the juvenile version of the Sand Crab. Practically means that this crab grows up INTO Sand Crab, but these small juveniles are far too weak to attack you normally. It also implies that Hermit Crabs are simply juvenile versions of Sand Crabs with a large barnacle on top of them.
+	- Walks
+	- Additional Notes
+	
+Health: 25
+Damage: 0
+Speed: 40 KM/H
+Armor: 0
+Family: Crab
+Type: Endemic Life
+Skills:
+	- Primary: Bubbles
+		- Release 2 bubbles that ensnare enemies. 
+            - CD: 2s
+	
+
+Log:
+TBD
+--]]
---
+++ Departed/Enemies/Creatures/Endemic Template.lua
@@ -0,0 +1,24 @@
+--[[
+Endemic Life Template
+Where It Appears
+Key Notes
+	- Endemic life are creatures that aren't typical enemies. They won't go out of their way to attack the player, and rather just exist in an area around the map. They won't respawn when killed, think the Birdsharks on Distant Roost. They are there to provide lore and information about an area. 
+	- What stages it appears on
+	- How it acts
+	- Biological factoids
+	- Movement Options (Walk, Jump, Fall, Climb, Fly, Teleport, None)
+	- Additional Notes
+	
+Health: 0
+Damage: 0
+Speed: 0 KM/H
+Armor: 0
+Family: Family Name Here
+Type: Endemic Life
+Skills:
+	- Misc:
+	
+
+Log:
+TBD
+--]]
