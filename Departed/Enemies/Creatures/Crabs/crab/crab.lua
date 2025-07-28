8c883528a3edff0d31c632fa8ab1fd463fac845f
Hobarticus "Whimsical Silly Road" Goobius <h0b4rt@gmail.com>
2025-07-28 07:45:29 -0500

    removed endemic life line from the crab file ToT

--- Departed/Enemies/Creatures/Crabs/crab/crab.lua
+++ Departed/Enemies/Creatures/Crabs/crab/crab.lua
@@ -2,7 +2,6 @@
 Crab
 ENDEMIC LIFE
 Key Notes
-	- Endemic life are creatures that aren't typical enemies. They won't go out of their way to attack the player, and rather just exist in an area around the map. They won't respawn when killed, think the Birdsharks on Distant Roost. They are there to provide lore and information about an area. 
 	- Appears on Dried Lake near the edges of the map, appears on Black Beach near the water.
 	- A skittish lil crab, implied to be the juvenile version of the Sand Crab. Practically means that this crab grows up INTO Sand Crab, but these small juveniles are far too weak to attack you normally. It also implies that Hermit Crabs are simply juvenile versions of Sand Crabs with a large barnacle on top of them.
 	- Walks
