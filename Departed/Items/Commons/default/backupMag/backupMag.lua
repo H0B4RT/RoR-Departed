bc83e0443e655a557ffa42af2b302d68c5453554
Hobarticus "Whimsical Silly Road" Goobius <h0b4rt@gmail.com>
2025-07-31 10:55:20 -0500

    Create backupMag.lua

---
+++ Departed/Items/Commons/default/backupMag/backupMag.lua
@@ -0,0 +1,32 @@
+--[[
+Backup Magazine
+Key Notes
+	- BASE CONTENT
+
+Unlock: Unlocked by default.
+Tier: Common
+Category: Utility
+Physical Description: A magazine of some kind of gun, presumably a rifle of some kind.
+Pick Up Text: Add an extra charge of your Secondary skill.
+Effect Description: Add +1 (+1 per stack) charge of your Secondary skill.
+
+Log:
+Shipping Details: 
+  Tracking Number: 177***********
+
+Order Details:
+
+It's not much, but our damaged spares in the back of the warehouse should be better than nothing. I'm really putting myself in a bad spot doing this outside of regulations, but the treaties make it tough to send out the good stuff. I can't let my buddies just die out there because of some big-wigs who are afraid to get the job done.
+Give em a few smacks before loading and fire a shot or two inside before trusting the mags. Good luck out there. At least we haven't seen any restrictions on the meds.
+
+Sent To:
+1803
+Combat Zone
+Venus
+
+Shipping Method:
+High Priority
+
+Estimated Delivery:
+9/12/2056
+--]]
