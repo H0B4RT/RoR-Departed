4cd8b2d5c300e86057939b97855cbfb1e11be073
Hobarticus "Whimsical Silly Road" Goobius <h0b4rt@gmail.com>
2025-07-31 11:06:04 -0500

    Update Item Template.lua
    forgot rework line lol

--- Departed/Items/Item Template.lua
+++ Departed/Items/Item Template.lua
@@ -2,6 +2,7 @@
 Item Name
 Key Notes
 	- What content pack their apart of (BASE CONTENT, DEPARTED CONTENT, STARSTORM CONTENT, FUTURE CONTENT)
+	- If it was changed or reworked from its original version, how was it changed?
 
 Unlock: If it is an unlock.
 Tier: 
