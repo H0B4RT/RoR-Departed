b6ff70a721d72b6b1cab26d9fe2f0b0873476b2e
Hobarticus "Whimsical Silly Road" Goobius <h0b4rt@gmail.com>
2025-07-31 10:59:41 -0500

    Updated Item Template.lua
    Wasn't satisfied with the original template, so I changed it.

--- Departed/Items/Item Template.lua
+++ Departed/Items/Item Template.lua
@@ -2,15 +2,13 @@
 Item Name
 Key Notes
 	- What content pack their apart of (BASE CONTENT, DEPARTED CONTENT, STARSTORM CONTENT, FUTURE CONTENT)
-	- What tier of item is it (Common, Uncommon, Legendary, Boss, Equipment, Relic, Special)
-	- DEFAULT or UNLOCK
-	- Physical Description
 
 Unlock: If it is an unlock.
-
+Tier: 
 Category:
+Physical Description:
 Pick Up Text:
-Item Description:
+Effect Description:
 
 Log:
 TBD
