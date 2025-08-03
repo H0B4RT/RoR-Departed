52908cef3c0c5dc1d4888c3bb9e6b4ff1df17dc3
Hobarticus "Whimsical Silly Road" Goobius <h0b4rt@gmail.com>
2025-08-02 20:34:46 -0500

    more organization le sigh AND construct A redesign OuO

---
+++ Departed/Items/Commons/default/armedBackpack/armedBackpack.lua
@@ -0,0 +1,16 @@
+--[[
+Armed Backpack
+Key Notes
+	- What content pack their apart of (BASE CONTENT, DEPARTED CONTENT, STARSTORM CONTENT, FUTURE CONTENT)
+	- If it was changed or reworked from its original version, how was it changed?
+
+Unlock: If it is an unlock.
+Tier: 
+Category:
+Physical Description:
+Pick Up Text:
+Effect Description:
+
+Log:
+TBD
+--]]
