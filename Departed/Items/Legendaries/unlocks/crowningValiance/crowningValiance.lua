687a1839e35a19573f55f3ede0f221ae3cd33ffd
H0B4RT <h0b4rt@gmail.com>
2025-12-06 23:08:15 -0600

    crowning valiance
    i love crowning valiance

--- Departed/Items/Legendaries/unlocks/crowningValiance/crowningValiance.lua
+++ Departed/Items/Legendaries/unlocks/crowningValiance/crowningValiance.lua
@@ -2,14 +2,14 @@
 Crowning Valiance
 Key Notes
 	- STARSTORM CONTENT
-	- The original Crowning Valiance was a green item that was kinda outclassed by Royal Medallion. So now, it's just kinda
+	- The original Crowning Valiance was a green item that was kinda outclassed by Royal Medallion. So now, it's just kinda permanent Crowning Valiance buff every time you kill a teleporter boss.
 
 Unlock: Defeat 25 bosses in a single run.
 Tier: Legendary
 Category: Damage / Utility / Survival
-Physical Description: An odd artifact that has a rope/chain holding it.
-Pick Up Text: All stats up upon killing a boss.
-Effect Description: Upon killing a boss, increase max health by +25% (+25% per stack), health regen by 0.2 (+0.2 per stack), damage by 20% (+20% per stack), attack speed by 20% (+20% per stack), critical strike chance by 10% (+10% per stack), movement speed by 15% (+15% per stack), and armor by 5 (+5 per stack).
+Physical Description: An odd artifact reaching out to a rope.
+Pick Up Text: All stats up upon killing a teleporter boss.
+Effect Description: Upon killing a teleporter boss, increase max health by +25% (+25% per stack), health regen by 0.2 (+0.2 per stack), damage by 20% (+20% per stack), attack speed by 20% (+20% per stack), critical strike chance by 10% (+10% per stack), movement speed by 15% (+15% per stack), and armor by 5 (+5 per stack).
 
 Log:
 Shipping Details:
@@ -17,16 +17,16 @@ Shipping Details:
 
 Order Details:
 
-X
+Remarkable object recovered from tomb #24. It is ingraved with a foreign language and seems to have carried a meaning of bravery or heroism. There are no signs of it being used for any functional purpose. Could have been merely for identification, although our devices display a dense flux of energy from within. Please confirm and fully detail the results before the deadline.
 
 Sent To:
-X,
-X,
-X
+Yoss H1,
+Sml,
+Saturn
 
 Shipping Method:
-X
+High Priority
 
 Estimated Delivery:
-X
+07/17/2056
 --]]
