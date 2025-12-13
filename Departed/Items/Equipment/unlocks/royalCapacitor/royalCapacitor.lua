9e1144a874bb705a07eb5fae1843393c7e4ac7fe
Hobarticus "Whimsical Silly Road" Goobius <h0b4rt@gmail.com>
2025-12-13 16:05:22 -0600

    lost doll

---
+++ Departed/Items/Equipment/unlocks/royalCapacitor/royalCapacitor.lua
@@ -0,0 +1,36 @@
+--[[
+Royal Capacitor
+Key Notes
+	- DEPARTED CONTENT
+	- Cooldown slightly nerfed.
+
+Unlock: Defeat the Teleporter bosses after activating 2 Mountain Shrines. (Ascendant)
+Tier: Equipment
+Category: Damage
+Cooldown: 30
+Physical Description: Its a lil orb with coils coming out of it and also some handles.
+Pick Up Text: Call down a lightning strike on a targeted monster.
+Effect Description: Call down a lightning strike on a targeted monster, dealing 3000% damage and stunning nearby monsters.
+Beating Embryo Effect: Effect happens twice.
+
+Log:
+Shipping Details:
+  Tracking Number: 1452***********
+
+Order Details:
+
+Here's a replacement for your damaged equipment. Crazy how gang activity is starting up again - even crazier that they'd steal a capacitor from you guys. 
+
+Anyway, you guys know the drill - make sure to routinely discharge the capacitor's storage cells, as it may hold on to latent voltage. Oh, and make sure to not point it at anybody when you do.
+
+Sent To:
+Power Plant 005,
+Route 10,
+Venus
+
+Shipping Method:
+Volatile
+
+Estimated Delivery:
+04/30/2056
+--]]
