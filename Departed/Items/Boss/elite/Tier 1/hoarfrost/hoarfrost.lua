42964d02f061123733c7a4e6b7e127fffa8bb1fe
Hobarticus "Whimsical Silly Road" Goobius <h0b4rt@gmail.com>
2025-12-24 20:22:20 -0600

    hoarfrost again
    still not finished

--- Departed/Items/Boss/elite/Tier 1/hoarfrost/hoarfrost.lua
+++ Departed/Items/Boss/elite/Tier 1/hoarfrost/hoarfrost.lua
@@ -1,15 +1,15 @@
 --[[
-Item Name
+Hoarfrost
 Key Notes
 	- DEPARTED CONTENT
 	- If it was changed or reworked from its original version, how was it changed?
 
-Acquisition: Slay an X Elite.
+Acquisition: Slay a glacial elite.
 Tier: Aspect
-Category: 
-Physical Description: 
-Pick Up Text: Become an aspect of x.
-Effect Description: 
+Category: Damage / Utility / Survival
+Physical Description: A miniature iceberg with permafrost underneath
+Pick Up Text: Become an aspect of ice.
+Effect Description: Attacks chill on hit, reducing movement speed by 50%. Attacks have a 5% (+5% per stack) chance to freeze. Upon reaching critical health, leave a freezing ice bomb that explodes after 2 seconds. This has a cooldown of 60 / 1 (+1 per stack) seconds.
 
 Log:
 Shipping Details:
